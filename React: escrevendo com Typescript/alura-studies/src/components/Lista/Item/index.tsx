0bd9cf3f7cb23e88a977d9fb756d0a7f243aecfe
Caroline P. da Silva <carolinepedasil@gmail.com>
2022-12-19 23:48:32 -0300

    State

--- React: escrevendo com Typescript/alura-studies/src/components/Lista/Item/index.tsx
+++ React: escrevendo com Typescript/alura-studies/src/components/Lista/Item/index.tsx
@@ -1,10 +1,12 @@
+import { ITarefa } from '../../../types/tarefa';
 import style from '../Lista.module.scss';
 
-export default function Item({ tarefa, tempo }: { tarefa: string, tempo: string }) {
-    return (
-        <li className={style.item}>
-        <h3> {tarefa} </h3>
-        <span> {tempo} </span>
+export default function Item({ tarefa, tempo, selecionado, completado, id }: ITarefa) {
+  console.log('item atual: ', { tarefa, tempo, selecionado, completado, id })
+  return (
+    <li className={style.item}>
+      <h3>{tarefa}</h3>
+      <span>{tempo}</span>
     </li>
-    )
+  )
 }
