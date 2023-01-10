ed428e96eda25321c99e1c58191790d3bea0d4cc
Caroline P. da Silva <carolinepedasil@gmail.com>
2023-01-10 20:45:36 -0300

    Manipulando os eventos

--- Alura/React com TypeScript/React: gerenciando estado com Recoil/event-tracker/src/App.tsx
+++ Alura/React com TypeScript/React: gerenciando estado com Recoil/event-tracker/src/App.tsx
@@ -26,7 +26,6 @@ function App() {
   //   }
   // ])
 
-
   const [filtro, setFiltro] = useState<Date | null>()
 
   const adicionarEvento = (evento: IEvento) => {
@@ -36,16 +35,16 @@ function App() {
     
     // setEventos([...eventos])
   }
-  const alterarStatusEvento = (id: number) => {
+  // const alterarStatusEvento = (id: number) => {
     // const evento = eventos.find(evento => evento.id === id)
     // if (evento) {
     //   evento.completo = !evento.completo
     // }
     // setEventos([...eventos])
-  }
-  const deletarEvento = (id: number) => {
-    // setEventos([...eventos.filter(evento => evento.id !== id)])
-  }
+  // }
+  // const deletarEvento = (id: number) => {
+  //   setEventos([...eventos.filter(evento => evento.id !== id)])
+  // }
 
   const aplicarFiltro = (data: Date | null) => {
     setFiltro(data)
@@ -67,9 +66,7 @@ function App() {
           <hr />
           <Card>
             <ListaDeEventos 
-              aoFiltroAplicado={aplicarFiltro} 
-              aoAlterarStatus={alterarStatusEvento} 
-              aoDeletarEvento={deletarEvento}
+              aoFiltroAplicado={aplicarFiltro}
             />
           </Card>
         </div>
--- Alura/React com TypeScript/React: gerenciando estado com Recoil/event-tracker/src/components/ListaDeEventos/index.tsx
+++ Alura/React com TypeScript/React: gerenciando estado com Recoil/event-tracker/src/components/ListaDeEventos/index.tsx
@@ -5,10 +5,7 @@ import style from './ListaDeEventos.module.scss';
 import { listaDeEventosState } from '../../state/atom';
 import { useRecoilValue } from 'recoil';
 
-const ListaDeEventos: React.FC<{ 
-  aoAlterarStatus: (id: number) => void, 
-  aoDeletarEvento: (id: number) => void, 
-  aoFiltroAplicado: (data: Date | null) => void }> = ({ aoDeletarEvento, aoAlterarStatus, aoFiltroAplicado }) => {
+const ListaDeEventos: React.FC<{ aoFiltroAplicado: (data: Date | null) => void }> = ({ aoFiltroAplicado }) => {
 
   const eventos = useRecoilValue(listaDeEventosState);
 
@@ -16,7 +13,7 @@ const ListaDeEventos: React.FC<{
     <Filtro aoFiltroAplicado={aoFiltroAplicado} />
     <div className={style.Scroll}>
       {eventos.map(evento => (
-        <Evento aoAlterarStatus={aoAlterarStatus} aoDeletarEvento={aoDeletarEvento} evento={evento} key={evento.id} />
+        <Evento evento={evento} key={evento.id} />
       ))}
     </div>
   </section>)
