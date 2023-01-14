cab93d4a610c11eec64922292c9bf224cfb55ee6
Caroline P. da Silva <carolinepedasil@gmail.com>
2023-01-14 18:41:41 -0300

    Seletores Async e Depuração

--- Alura/React com TypeScript/React: gerenciando estado com Recoil/event-tracker/src/components/Calendario/index.tsx
+++ Alura/React com TypeScript/React: gerenciando estado com Recoil/event-tracker/src/components/Calendario/index.tsx
@@ -1,10 +1,11 @@
+
 import React from 'react'
 import style from './Calendario.module.scss';
 import ptBR from './localizacao/ptBR.json'
-import Kalend, { CalendarEvent, CalendarView, OnEventDragFinish } from 'kalend'
+import Kalend, { CalendarView } from 'kalend'
 import 'kalend/dist/styles/index.css';
-import useAtualizarEvento from '../../state/hooks/useAtualizarEvento';
-import useListaDeEventos from '../../state/hooks/useListaDeEventos';
+import { useRecoilValue } from 'recoil';
+import { listaDeEventosState } from '../../state/atom';
 
 interface IKalendEvento {
   id?: number
@@ -17,8 +18,7 @@ interface IKalendEvento {
 const Calendario: React.FC = () => {
 
   const eventosKalend = new Map<string, IKalendEvento[]>();
-  const eventos = useListaDeEventos()
-  const atualizarEvento = useAtualizarEvento()
+  const eventos = useRecoilValue(listaDeEventosState);
 
   eventos.forEach(evento => {
     const chave = evento.inicio.toISOString().slice(0, 10)
@@ -33,22 +33,6 @@ const Calendario: React.FC = () => {
       color: 'blue'
     })
   })
-
-  const onEventDragFinish: OnEventDragFinish = (
-    kalendEventoInalterado: CalendarEvent,
-    kalendEventoAtualizado: CalendarEvent
-  ) => {
-    const evento = eventos.find(evento => evento.descricao === kalendEventoAtualizado.summary)
-    if (evento) {
-      const eventoAtualizado = {
-        ...evento
-      }
-      eventoAtualizado.inicio = new Date(kalendEventoAtualizado.startAt)
-      eventoAtualizado.fim = new Date(kalendEventoAtualizado.endAt)
-      atualizarEvento(eventoAtualizado)
-    }
-  };
-
   return (
     <div className={style.Container}>
       <Kalend
@@ -61,7 +45,6 @@ const Calendario: React.FC = () => {
         calendarIDsHidden={['work']}
         language={'customLanguage'}
         customLanguage={ptBR}
-        onEventDragFinish={onEventDragFinish}
       />
     </div>
   );
